1ecfa3a99c135ddce7c310d439fc3d3264e75bc8
user <user@gmail.com>
2015-04-27 20:22:06 +1200

    added query to piet's server

---
+++ GlassHarpoon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace GlassHarpoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://192.168.1.3:9393/");
+                HttpResponseMessage response = client.GetAsync("help").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    JObject whatIsThis = response.Content.ReadAsAsync<JObject>().Result;
+                    Console.WriteLine(whatIsThis);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
